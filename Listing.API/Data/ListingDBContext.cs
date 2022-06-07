6c479a62548a15de8e5f7d2cd537ae9308605609
Gopi <kataigopi@gmail.com>
2022-06-07 22:36:02 +0530

    add dbContext ,connectionStrings

---
+++ Listing.API/Data/ListingDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Listing.API.Data
+{
+    public class ListingDBContext : DbContext
+    {
+        public ListingDBContext(DbContextOptions options):base(options)
+        {
+
+        }
+    }
+}
--- Listing.API/Program.cs
+++ Listing.API/Program.cs
@@ -1,6 +1,17 @@
+using Listing.API.Data;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+var connectionString = builder.Configuration.GetConnectionString("ListingDbConnectionString");
+builder.Services.AddDbContext<ListingDBContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -14,8 +25,13 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAll", b =>
     {
         b.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
+       
     });
 });
+
+builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
+
+
 
 var app = builder.Build();
 
